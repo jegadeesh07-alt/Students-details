be96eb588765cfc095c624f255f0b140ecd5e864
jegadeesh07-alt <jegadeesh200421@gmail.com>
2025-04-28 13:00:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import "./index.css"
 function App() {
   return (
     <div className="App">
-     <div className="logo">
-      <img className="log-image" src="https://logodownload.org/wp-content/uploads/2014/04/amazon-logo-0.png" alt="can't reload " />
-     </div>
+
     <SignupForm />
 
     </div>
